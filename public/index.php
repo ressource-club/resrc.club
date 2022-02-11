cce8d428b2037b3fb0d84b92e04315b33d51a1e3
Clément Mauperon <47110246+tensaiji@users.noreply.github.com>
2022-02-11 08:48:31 -0500

    architecture

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+define('__RACINE__', realpath(__DIR__ . "/../") . '/');
+
+include __RACINE__ . 'html/composants/entete.html';
+
+
+include __RACINE__ . 'html/citation.html';
+
+
+include __RACINE__ . 'html/composants/pied.html';
+
+?>
